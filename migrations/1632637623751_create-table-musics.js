df882667b1d01f28dd8628009f66b26f56756424
Eryan Fauzan <eryangamer10@gmail.com>
2021-09-26 13:46:55 +0700

    feat: initial commit project skeleton

---
+++ migrations/1632637623751_create-table-musics.js
@@ -0,0 +1,42 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.createTable('musics', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    year: {
+        type: 'SMALLINT',
+        notNull: true,
+    },
+    performer: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    genre: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    duration: {
+        type: 'SMALLINT',
+        notNull: true,
+    },
+    created_at: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = pgm => {};
